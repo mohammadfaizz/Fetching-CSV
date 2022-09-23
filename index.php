ac3336d863654b059f39c777ed3a00aba802f249
anonimous <anonimous@somewhere.com>
2022-09-23 10:37:17 +0530

    adding one more csv file and modifies the .php file to insert it to DataBase

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 // root is the username,
 // password is empty
 // database name is gfg
-$db = new mysqli('localhost', 'root', '', 'gfg');
+$db = new mysqli('localhost', 'root', '', 'employees');
 
 // Checking connection
 if ($db->connect_errno) {
@@ -17,12 +17,14 @@ exit();
 		<?php
 
 		// Get csv file
-		if(($handle = fopen("detail.csv","r")) !== FALSE) {
+		if(($handle = fopen("employees.csv","r")) !== FALSE) {
 			while(($row = fgetcsv($handle)) !== FALSE) {
 
-				$db->query('INSERT INTO table2 VALUES ("'.$row[0].'","'.$row[1].'", "'.$row[2].'")') ;
+				$db->query('INSERT INTO details VALUES (NULL,"'.$row[0].'","'.$row[1].'", "'.$row[2].'","'.$row[3].'","'.$row[4].'", "'.$row[5].'","'.$row[6].'","'.$row[7].'", "'.$row[8].'","'.$row[9].'","'.$row[10].'")');
 			
 			}
+
+			echo "Successfully Inserted";
 			
 		// Closing the file
 		fclose($handle);
